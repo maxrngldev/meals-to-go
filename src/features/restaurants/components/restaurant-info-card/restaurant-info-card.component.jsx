7cb9a05c67fc04e507b43de3c514b0488856026a
maxrngldev <maxrngl08@gmail.com>
2023-05-05 01:52:38 -0600

    rendering list of cards

--- src/features/restaurants/components/restaurant-info-card/restaurant-info-card.component.jsx
+++ src/features/restaurants/components/restaurant-info-card/restaurant-info-card.component.jsx
@@ -17,19 +17,16 @@ import {
 import star from '../../../../../assets/star';
 import open from '../../../../../assets/open';
 
-export default function RestaurantInfoCard({ restaurant = {} }) {
+export default function RestaurantInfoCard({ restaurant }) {
   const {
-    name = 'Some Restaurant',
-    icon = 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
-    photos = [
-      'https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg',
-    ],
-    address = '100 Some Random Street',
-    isOpenNow = true,
-    rating = 4,
-    isClosedTemporarily = true,
+    rating,
+    name,
+    photos,
+    isClosedTemporarily,
+    isOpenNow,
+    icon,
+    address,
   } = restaurant;
-
   const ratingArray = Array.from(new Array(Math.ceil(rating)));
 
   return (
--- src/features/restaurants/screens/restaurants.screen.jsx
+++ src/features/restaurants/screens/restaurants.screen.jsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, StatusBar, StyleSheet, View } from 'react-native';
+import { SafeAreaView, StatusBar, View, FlatList } from 'react-native';
 import { Searchbar } from 'react-native-paper';
 import styled from 'styled-components';
 
@@ -14,14 +14,61 @@ const SearchContainer = styled(View)`
   padding: ${props => props.theme.space[3]};
 `;
 
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: { padding: 16 },
+})``;
+
 export default function RestaurantsScreen() {
+  const data = [
+    {
+      id: 1,
+      name: 'Some Restaurant',
+      icon: 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
+      photos: [
+        'https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg',
+      ],
+      address: '100 Some Random Street',
+      isOpenNow: true,
+      rating: 4,
+      isClosedTemporarily: true,
+    },
+    {
+      id: 2,
+      name: 'Some Restaurant',
+      icon: 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
+      photos: [
+        'https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg',
+      ],
+      address: '100 Some Random Street',
+      isOpenNow: true,
+      rating: 2,
+      isClosedTemporarily: false,
+    },
+    {
+      id: 3,
+      name: 'Some Restaurant',
+      icon: 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
+      photos: [
+        'https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg',
+      ],
+      address: '100 Some Random Street',
+      isOpenNow: true,
+      rating: 5,
+      isClosedTemporarily: true,
+    },
+  ];
   return (
     <SafeArea>
       <SearchContainer>
         <Searchbar />
       </SearchContainer>
 
-      <RestaurantInfoCard />
+      <RestaurantList
+        data={data}
+        renderItem={({ item }) => <RestaurantInfoCard restaurant={item} />}
+        keyExtractor={item => item.id}
+        contentContainerStyle={{ padding: 16 }}
+      ></RestaurantList>
     </SafeArea>
   );
 }
